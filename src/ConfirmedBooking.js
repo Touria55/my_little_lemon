16223073dadbcdd2687a24d1ceaed3a0b31b1564
kaoutar ouhmid <touriatouria6675@gmail.com>
2023-10-30 14:26:35 +0100

    first commit

---
+++ src/ConfirmedBooking.js
@@ -0,0 +1,9 @@
+const ConfirmedBooking=()=>{
+    return(
+        <div className="confirmh">
+            <h1>Booking has been  <span> confirmed ! </span> &#128522; </h1>
+        </div>
+    )
+}
+
+export default ConfirmedBooking;
